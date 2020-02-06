5bd0a697ed7a9138e42f08dd7b5ebe03ee8c004a
Maks Luniak <pi59_lmo@student.ztu.edu.ua>
2020-02-06 14:34:42 +0200

    80%

--- simple.js
+++ simple.js
@@ -4,3 +4,82 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
 });
+
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующй слайд*/
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+
+/* Функция уменьшяет индекс на 1, показывает предыдущий слайд*/
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+// $(window).bind('scroll', function() {
+//     var currentTop = $(window).scrollTop();
+//     var elems = $('.scrollspy');
+//     elems.each(function(index){
+//       var elemTop 	= $(this).offset().top;
+//       var elemBottom 	= elemTop + $(this).height();
+//       if(currentTop >= elemTop && currentTop <= elemBottom){
+//         var id 		= $(this).attr('id');
+//         var navElem = $('a[href="#' + id+ '"]');
+//     navElem.parent().addClass('active-nav').siblings().removeClass( 'active-nav' );
+//       }
+//     })
+// }); 
+
+$(function() {
+    $('#navigation').navpoints({offset: 50});
+});
+
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('macphoto');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
